bca48075f09a339fc6ac45034724065a9b800b84
irebasunglass <irebasunglass213@gmail.com>
2024-01-17 16:42:09 +0900

    フロント部分の変更

---
+++ static/script.js
@@ -0,0 +1,22 @@
+
+const dire_Mark = document.getElementById('direction_mark');
+console.log(dire_Mark)
+const ctx = dire_Mark.getContext('2d');
+
+function draw_d_m() {
+    ctx.strokeStyle = "red";
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.moveTo(0, 69);
+    ctx.quadraticCurveTo(100, 20, 200, 69);
+    ctx.lineTo(220, 49);
+    ctx.lineTo(220, 109);
+    ctx.lineTo(150, 129);
+    ctx.lineTo(165, 109);
+    ctx.quadraticCurveTo(100, 60, 10, 89);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+}
+
+draw_d_m();
